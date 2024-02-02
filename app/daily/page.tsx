7918ca8fed5ebc201ccce585c1274b6368f51ef9
Emily Daitch <emily.daitch@gmail.com>
2024-02-02 17:52:43 -0500

    display first incomplete daily task

--- app/daily/page.tsx
+++ app/daily/page.tsx
@@ -4,25 +4,50 @@ import { Title, Text } from '@tremor/react';
 interface DailyLog {
   id: number;
   description: string;
-  lastVisit: Date;
-  nextVisit: Date;
-  username: String;
+  username: string;
+  taskid: string;
 }
 
+interface DailyTask {
+    id: string;
+    description: string;
+    username: string;
+    taskorder: number;
+  }
+
 export default async function DailyPage() {
-  const result = await sql`
+  const logResult = await sql`
     SELECT id, taskid, done, username
     FROM dailylog 
     WHERE username ILIKE  'edaitch'
-    AND   day      =      now()::date;
+    AND   day      =      now()::date
+    ORDER BY id DESC;
   `;
-  const dailyLogs = result.rows as DailyLog[];
+  const dailyLogs = logResult.rows as DailyLog[];
+
+  const taskResult = await sql`
+    SELECT id, username, taskorder, description
+    FROM dailytask
+    WHERE username ILIKE  'edaitch'
+    ORDER BY taskorder ASC;
+  `;
+  const dailyTasks = taskResult.rows as DailyTask[];
+
+  let dailyTaskNumber = 1;
+  let lastDoneTaskId = '';
+  console.log('dailylogs', dailyLogs);
+  if(dailyLogs.length == 0) {
+    console.log('daily logs empty');
+  } else {
+    lastDoneTaskId = dailyLogs[dailyLogs.length - 1].taskid;
+  }
 
   return (
     <main className="p-4 md:p-10 mx-auto max-w-7xl">
       <Title>Daily</Title>
       <Text>Daily task prompts, ordered.</Text>
-      {dailyLogs[0]?.username}
+      {dailyLogs.length != 0 ? dailyTasks[dailyTasks.findIndex((task) => task.id = lastDoneTaskId)].description
+                 : dailyTasks[dailyTasks.findIndex((task) => task.taskorder = dailyTaskNumber)].description}
     </main>
   );
 }
