17518ddd218d6abeb41aa021e1b0c0ecd57eb413
Emily Daitch <emily.daitch@gmail.com>
2024-02-21 20:22:59 -0500

    chunk and backpressure streaming examples

--- app/demos/streaming/page.tsx
+++ app/demos/streaming/page.tsx
@@ -1,11 +1,17 @@
-import { Title, } from '@tremor/react';
+'use client';
+import { Title, Button } from '@tremor/react';
 
 export default async function StreamingDemo() {
-  await fetch('https://admin-dashboard-tailwind-postgres-react-nextjs-ruby-eta.vercel.app/api/handle-backpressure');
-  await fetch('https://admin-dashboard-tailwind-postgres-react-nextjs-ruby-eta.vercel.app/api/chunk-example');
+  //await fetch('https://admin-dashboard-tailwind-postgres-react-nextjs-ruby-eta.vercel.app/api/handle-backpressure');
+  //await fetch('https://admin-dashboard-tailwind-postgres-react-nextjs-ruby-eta.vercel.app/api/chunk-example');
   return (
     <main className="p-4 md:p-10 mx-auto max-w-7xl">
       <Title>Streaming Demo</Title>
-    </main>
+      <Button onClick={async () => {
+        await fetch('https://admin-dashboard-tailwind-postgres-react-nextjs-ruby-eta.vercel.app/api/handle-backpressure');
+      }}>Backpressure</Button>
+      <Button onClick={async () => {
+        await fetch('https://admin-dashboard-tailwind-postgres-react-nextjs-ruby-eta.vercel.app/api/chunk-example');
+      }}>Chunk</Button>    </main>
   );
 }
