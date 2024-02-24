4c13395eabd575091edf25edfd770ddafc466935
Emily Daitch <emily.daitch@gmail.com>
2024-02-23 21:28:31 -0500

    fix upsert

--- app/api/appointments/route.ts
+++ app/api/appointments/route.ts
@@ -24,7 +24,7 @@ let updateAppointments = async function(appointment: Partial<Appointment>) {
   const result = await sql`INSERT INTO appointments (id, description, lastVisit, nextVisit)
   VALUES (${appointment.id}, ${appointment.description}, ${appointment.lastVisit?.toISOString()}, ${appointment.nextVisit?.toISOString()})
   ON CONFLICT (id)
-  DO NOTHING | DO UPDATE SET description = ${appointment.description}, lastVisit = ${appointment.lastVisit?.toISOString()}, nextVisit = ${appointment.nextVisit?.toISOString()};`;
+  DO UPDATE SET description = ${appointment.description}, lastVisit = ${appointment.lastVisit?.toISOString()}, nextVisit = ${appointment.nextVisit?.toISOString()};`;
   const appointments = result.rows as Appointment[];
   // should only return updated task, not bulk update... TODO
   console.log('result from update appointment', result);
